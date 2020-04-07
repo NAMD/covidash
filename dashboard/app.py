2c85aa81d6cdab1175d5644c7a68d2ce829d5ca8
Flávio Codeço Coelho <fccoelho@gmail.com>
2020-04-07 12:10:31 -0300

    simulation plot working

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,8 +1,28 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+from epimodels.continuous.models import SEQIAHR
 
 
 def main():
-    pass
+    traces = pd.DataFrame(data=run_model())
+    traces.set_index('time', inplace=True)
+
+    st.line_chart(traces)
+
+
+@st.cache
+def run_model(inits=[97.3e6, 0, 1, 0, 0, 0, 0], trange=[0, 365], N=97.3e6,
+              params={'chi': .3, 'phi': .01, 'beta': .5,
+                      'rho': 1, 'delta': .1, 'alpha': 2,
+                      'p': .75, 'q': 30
+                      }):
+    model = SEQIAHR()
+    model(inits=inits, trange=trange, totpop=N, params={'chi': .3, 'phi': .01, 'beta': .5,
+                                                        'rho': 1, 'delta': .1, 'alpha': 2,
+                                                        'p': .75, 'q': 30
+                                                        })
+    return model.traces
 
 
 @st.cache
